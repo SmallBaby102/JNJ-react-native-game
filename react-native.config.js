b1eb2b30b6908a155440e857518e4715079bfa9e
smallbaby102 <smallbaby102@outlook.com>
2022-07-08 17:52:43 +1000

    update

---
+++ react-native.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {}, // grouped into "project"
+  },
+  assets: ["./src/fonts/Grenadine", "./src/fonts/Objektiv", "./src/fonts/test"] // stays the same
+};
